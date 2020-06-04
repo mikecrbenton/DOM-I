38965d7df6575596723391736ee589c201818ab0
mikecrbenton <mikecrbenton@hotmail.com>
2020-06-03 20:23:24 -0500

    added comments cleaned up code

--- js/index.js
+++ js/index.js
@@ -49,6 +49,7 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 
 //======================================================
+// SET THE 6 <a> LINKS IN <nav>
 
 let navBttn = document.querySelector('nav').children;  
 navBttn = Array.from(navBttn);
@@ -63,39 +64,36 @@ for ( key in siteContent.nav) {
    }
 }
 
-// navBttn.forEach( (eachTag) => {                        //---------------------------------------
-//    eachTag.innerHTML = siteContent.nav['nav-item-1'];  // how to access JSON dynamically inside the loop??
-                                                          //----------------------------------------
-// })
 
 
 //======================================================
+// SET THE HEADER ( STILL NEED TO ADD <br> )
 
-//NEED TO ADD <br>!!!!
 let h1_el = document.querySelector("h1");
 console.log(h1_el);
 h1_el.innerHTML = siteContent["cta"]['h1'];
 
 
+
 //======================================================
+// BUTTON
 
 let bttn_el = document.querySelector("button");
 bttn_el.innerHTML = siteContent["cta"]["button"];
 
 
 //======================================================
+// IMAGE HEADER
 
 let mainImg = document.getElementById("cta-img");
 mainImg.src = "img/header-img.png";
 
 
 //======================================================
-
-//GRAB THE HTML ELEMENTS ( THIS DOES NOT GRAB THE IMAGE!! )
-let contentBlocks = document.querySelectorAll(".text-content"); 
+// PARAGRAPHS AND HEADERS
 
 /* ------------------------------------------------
-   Loop through the inner JSON object- placing into
+   Loop through the inner JSON object- placing values into
    the NodeList "contentBlocks" from index.html
    It has 5 divs, each with 2 html elements - 
    Example [0][0] and [0][1] then [1][0] and [1][1] 
@@ -103,31 +101,37 @@ let contentBlocks = document.querySelectorAll(".text-content");
    for handling the image from the JSON object )
    ------------------------------------------------*/
 
+//GRAB THE HTML ELEMENTS ( THIS DOES NOT GRAB THE IMAGE!! )
+let contentBlocks = document.querySelectorAll(".text-content"); 
+
 let currentDiv = 0;
 let htmlElements = 0;
 
 for ( key in siteContent['main-content']) {
 
-      //WHEN IT HITS THE IMAGE, HANDLE SEPARATELY
+      //HANDLE IMAGE SEPARATELY
       if( key === 'middle-img-src'){
+
          document.getElementById('middle-img').src = siteContent['main-content'][key];
-         /* The image is only 1 html element - not 2 , so no need to increment htmlElements */
+         /* The image is only 1 html element - not 2 , so no need to increment for 2 htmlElements
+            - it can stay set at 0 for the next <div> */
+
       }else{
          contentBlocks[ currentDiv ].children[ htmlElements ].textContent = siteContent['main-content'][key];
-         htmlElements++;
+         htmlElements++;//
       }
        
-      //RESET EVERY 2 LOOPS
+      //RESET EVERY 2 LOOPS ( OR EACH DIV)
       if( htmlElements > 1 ) { 
-         htmlElements = 0
-         currentDiv++; 
+         htmlElements = 0  // THIS RESETS TO 0 AFTER EVERY 2 PASSES FOR THE NEXT SET OF AN <h4> & <p>
+         currentDiv++;     // THIS INCRIMENTS AFTER 2 PASSES FOR THE NEXT DIV
       };
 }
 
 
 
-
 //======================================================
+// CONTACT BLOCK
 
 let contact = document.getElementsByClassName('contact');
 
@@ -140,18 +144,17 @@ for( key in siteContent['contact']){
    i++
 }
 
-console.log( contact );
-
-
 
 
 //======================================================
+// COPYRIGHT
 
 let footer = document.querySelector('footer p');
 footer.textContent = siteContent['footer']['copyright'];
 
 
 //=======================================================
+// CHANGE LINKS TO GREEN
 
 let navToGreen = document.querySelectorAll('nav a');
 //console.log(navToGreen);
@@ -161,6 +164,7 @@ navToGreen.forEach( (eachTag) => {
 });
 
 //==========================================================
+// ADD NEW LINKS
 
 // CREATE HTML ELEMENT
 let newFrontElement = document.createElement('a');
@@ -171,14 +175,18 @@ let newFrontText = document.createTextNode('Front');
 let newEndText = document.createTextNode('End');
 
 // APPEND CONTENT AND ADD ATTRIBUTES
-newFrontElement.append(newFrontText);
-newFrontElement.setAttribute('href', '#');
-newFrontElement.style.color = 'green';
-
-newEndElement.append(newEndText);
-newEndElement.setAttribute('href', '#');
-newEndElement.style.color = 'green';
+createNewElement(newFrontElement,newFrontText);
+createNewElement(newEndElement,newEndText);
 
 // ADD TO THE DOCUMENT
 document.querySelectorAll('nav')[0].prepend(newFrontElement);
 document.querySelectorAll('nav')[0].appendChild(newEndElement);
+
+
+
+// HELPER FUNCTION
+function createNewElement(typeOfElement, newContent ){
+   typeOfElement.append(newContent);
+   typeOfElement.setAttribute('href', '#');
+   typeOfElement.style.color = 'green';
+}
